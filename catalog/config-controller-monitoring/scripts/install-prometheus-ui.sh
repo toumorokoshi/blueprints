1777ea4187e2148a6c13c336d84d6f539b606aa0
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2022-03-01 11:47:11 -0800

    cc-monitoring: prometheus-ui install by script
    The config-controller-monitoring blueprint should install it's
    prometheus-ui by script only.
    
    Leaving the .yaml prefix will result in kpt attempting to gain
    ownership, which will fail apply due to kpt not seeing the
    owning-inventory labels:
    
    ```
    deployment.apps/frontend apply failed: can't adopt an object without the annotation config.k8s.io/owning-inventory
    ```

--- catalog/config-controller-monitoring/scripts/install-prometheus-ui.sh
+++ catalog/config-controller-monitoring/scripts/install-prometheus-ui.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 kubectl delete validatingwebhookconfigurations/gatekeeper-validating-webhook-configuration -A
-kubectl apply -f ./scripts/prometheus-ui.yaml
+kubectl apply -f ./scripts/prometheus-ui.yaml.install-by-script
